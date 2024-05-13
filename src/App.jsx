44a7adadf27d43591bfd7a795887415649f73dd5
Alexeytyurin143 <alexsei89tyrin@gmail.com>
2024-05-13 19:25:38 +0400

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,101 @@
-import { useState } from "react";
-import "./App.css";
-import { PersonForm } from "./PersonForm";
+import { useState } from 'react'
+import './App.css'
+import { PersonForm } from './PersonForm'
 
 function App() {
-  const [isFormVisible, setIsFormVisible] = useState(false);
+	const [isFormVisible, setIsFormVisible] = useState(false)
+	const [currentUser, setCurrentUser] = useState({
+		id: '',
+		fullName: '',
+		birthdate: '',
+		hobbies: '',
+		gender: '',
+		image: null,
+	})
 
-  const [users, setUsers] = useState([]);
+	const [users, setUsers] = useState([])
 
-  const addUser = (user) => {
-    setUsers([...users, user]);
-  };
+	const addUser = (user) => {
+		setUsers([...users, user])
+	}
 
-  return (
-    <>
-      <div className="container">
-        <button
-          style={{ margin: "50px 0" }}
-          onClick={() => setIsFormVisible(true)}
-        >
-          Добавить пользователя
-        </button>
-        <div>
-          <h2 style={{ fontSize: 25, marginBottom: 20 }}>Список</h2>
-        </div>
-        <table>
-          <thead>
-            <tr>
-              <th>ФИО</th>
-              <th>Дата рождения</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((user) => (
-              <tr key={user.id}>
-                <td>{user.fullName}</td>
-                <td>{user.birthdate}</td>
-                <td>
-                  <button style={{ marginRight: 15 }}>Редактировать</button>
-                  <button>Удалить</button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+	const deleteUser = (userId) => {
+		const usersNew = users.filter((user) => user.id !== userId)
+		setUsers(usersNew)
+	}
 
-      <PersonForm
-        isFormVisible={isFormVisible}
-        setIsFormVisible={setIsFormVisible}
-        addUser={addUser}
-      />
-    </>
-  );
+	const editUser = (userToEdit) => {
+		setUsers((prevUsers) =>
+			prevUsers.map((user) =>
+				user.id === userToEdit.id ? userToEdit : user
+			)
+		)
+	}
+
+	return (
+		<>
+			<div className='container'>
+				<button
+					style={{ margin: '50px 0' }}
+					onClick={() => setIsFormVisible(true)}
+				>
+					Добавить пользователя
+				</button>
+				<div>
+					<h2 style={{ fontSize: 25, marginBottom: 20 }}>Список</h2>
+				</div>
+				<table>
+					<thead>
+						<tr>
+							<th>ФИО</th>
+							<th>Дата рождения</th>
+							<th></th>
+						</tr>
+					</thead>
+					<tbody>
+						{users.map((user) => (
+							<tr key={user.id}>
+								<td>{user.fullName}</td>
+								<td>{user.birthdate}</td>
+								<td>
+									<button
+										onClick={(e) => {
+											e.preventDefault()
+											setCurrentUser({
+												...user,
+												hobbies: user.hobbies.join(),
+											})
+											setIsFormVisible(true)
+										}}
+										style={{ marginRight: 15 }}
+									>
+										Редактировать
+									</button>
+									<button
+										onClick={(e) => {
+											e.preventDefault()
+											deleteUser(user.id)
+										}}
+									>
+										Удалить
+									</button>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
+
+			<PersonForm
+				isFormVisible={isFormVisible}
+				setIsFormVisible={setIsFormVisible}
+				addUser={addUser}
+				editUser={editUser}
+				currentUser={currentUser}
+				setCurrentUser={setCurrentUser}
+			/>
+		</>
+	)
 }
 
-export default App;
+export default App
--- src/PersonForm.jsx
+++ src/PersonForm.jsx
@@ -1,170 +1,336 @@
-import { useState } from "react";
+import jsPDF from 'jspdf'
+import { useRef, useState } from 'react'
 
-export const PersonForm = ({ isFormVisible, setIsFormVisible, addUser }) => {
-  const [fullName, setFullName] = useState("");
-  const [birthdate, setBirthdate] = useState("");
-  const [hobbies, setHobbies] = useState("");
-  const [gender, setGender] = useState("");
-  const [photo, setPhoto] = useState(null);
-  const [currentId, setCurrentId] = useState(1);
+export const PersonForm = ({
+	isFormVisible,
+	setIsFormVisible,
+	addUser,
+	editUser,
+	setCurrentUser,
+	currentUser,
+}) => {
+	const [currentId, setCurrentId] = useState(1)
+	const videoRef = useRef(null)
+	const [mediaStream, setMediaStream] = useState(null)
 
-  const capturePhoto = async (e) => {
-    e.preventDefault();
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({
-        video: true,
-      });
-      const videoElement = document.createElement("video");
-      videoElement.srcObject = stream;
-      videoElement.play();
-      const canvas = document.createElement("canvas");
-      canvas.width = videoElement.videoWidth;
-      canvas.height = videoElement.videoHeight;
-      const context = canvas.getContext("2d");
-      context.drawImage(videoElement, 0, 0, canvas.width, canvas.height);
-      const dataUrl = canvas.toDataURL("image/jpeg");
-      setPhoto(dataUrl);
-      stream.getVideoTracks()[0].stop();
-    } catch (error) {
-      console.error("Error capturing photo:", error);
-    }
-  };
+	const { id, fullName, birthdate, hobbies, gender, image } = currentUser
 
-  const exportToJson = () => {
-    const personData = {
-      fullName,
-      birthdate,
-      hobbies: hobbies.split(",").map((hobby) => hobby.trim()),
-    };
-    const jsonData = JSON.stringify(personData, null, 2);
-    const blob = new Blob([jsonData], { type: "application/json" });
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = "personData.json";
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-    URL.revokeObjectURL(url);
-  };
+	const startVideo = async () => {
+		try {
+			const stream = await navigator.mediaDevices.getUserMedia({
+				video: true,
+			})
+			setMediaStream(stream)
+			if (videoRef.current) {
+				videoRef.current.srcObject = stream
+			}
+		} catch (err) {
+			console.error('Error accessing webcam:', err)
+		}
+	}
 
-  const exportToPdf = () => {
-    const personData = `Full Name: ${fullName}\nBirthdate: ${birthdate}\nHobbies: ${hobbies}`;
-    const pdfContent = `
-      <html>
-        <head>
-          <title>Person Data</title>
-        </head>
-        <body>
-          <pre>${personData}</pre>
-        </body>
-      </html>
-    `;
-    const blob = new Blob([pdfContent], { type: "application/pdf" });
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = "personData.pdf";
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-    URL.revokeObjectURL(url);
-  };
+	const captureImage = () => {
+		if (videoRef.current) {
+			const canvas = document.createElement('canvas')
+			canvas.width = videoRef.current.videoWidth
+			canvas.height = videoRef.current.videoHeight
+			const context = canvas.getContext('2d')
+			context.drawImage(
+				videoRef.current,
+				0,
+				0,
+				canvas.width,
+				canvas.height
+			)
 
-  if (!isFormVisible) {
-    return;
-  }
+			const dataURL = canvas.toDataURL('image/png')
 
-  const handleAddUser = (e) => {
-    e.preventDefault();
-    const personData = {
-      id: currentId,
-      fullName,
-      birthdate,
-      gender,
-      hobbies: hobbies.split(",").map((hobby) => hobby.trim()),
-    };
-    addUser(personData);
-    setCurrentId(currentId + 1);
-    setFullName("");
-    setBirthdate("");
-    setHobbies("");
-    setGender("");
-    setPhoto("");
+			setCurrentUser({ ...currentUser, image: dataURL })
 
-    setIsFormVisible(false);
-  };
+			mediaStream.getTracks().forEach((track) => track.stop())
+		}
+	}
 
-  return (
-    <div className="personFormWrapper">
-      <form className="personForm" id="personForm">
-        <label>
-          ФИО
-          <input
-            type="text"
-            placeholder="ФИО"
-            value={fullName}
-            onChange={(e) => setFullName(e.target.value)}
-          />
-        </label>
+	const retakeImage = () => {
+		setCurrentUser({ ...currentUser, image: null })
+		startVideo()
+	}
 
-        <label>
-          Дата рождения
-          <input
-            type="date"
-            placeholder="Дата рождения"
-            value={birthdate}
-            onChange={(e) => setBirthdate(e.target.value)}
-          />
-        </label>
+	const exportToJson = (e) => {
+		e.preventDefault()
+		const personData = {
+			fullName,
+			birthdate,
+			hobbies: hobbies.split(',').map((hobby) => hobby.trim()),
+		}
+		const jsonData = JSON.stringify(personData, null, 2)
+		const blob = new Blob([jsonData], { type: 'application/json' })
+		const url = URL.createObjectURL(blob)
+		const a = document.createElement('a')
+		a.href = url
+		a.download = 'personData.json'
+		document.body.appendChild(a)
+		a.click()
+		document.body.removeChild(a)
+		URL.revokeObjectURL(url)
+	}
 
-        <fieldset>
-          <legend>Выберите пол</legend>
-          <div>
-            <label>
-              <input
-                type="radio"
-                value="male"
-                name="gender"
-                onChange={(e) => setGender(e.target.value)}
-              />
-              Мужской
-            </label>
-            <label>
-              <input
-                type="radio"
-                value="female"
-                name="gender"
-                onChange={(e) => setGender(e.target.value)}
-              />
-              Женский
-            </label>
-          </div>
-        </fieldset>
+	const exportToPdf = (e) => {
+		e.preventDefault()
+		const doc = new jsPDF()
+		doc.text(`Full name: ${fullName}`, 10, 10)
+		doc.text(`Date of birth: ${birthdate}`, 10, 20)
+		doc.text(`Gender: ${gender}`, 10, 30)
+		doc.text(`Hobbies: ${hobbies}`, 10, 40)
+		if (image) {
+			const img = new Image()
+			img.src = image
+			img.onload = function () {
+				const canvas = document.createElement('canvas')
+				const ctx = canvas.getContext('2d')
+				canvas.width = this.width
+				canvas.height = this.height
+				ctx.drawImage(this, 0, 0)
+				const dataUrl = canvas.toDataURL('image/jpeg')
+				doc.addImage(dataUrl, 'JPEG', 10, 50, 100, 75)
+				doc.save('personData.pdf')
+			}
+		} else {
+			doc.save('personData.pdf')
+		}
+	}
 
-        <label>
-          Увлечения
-          <input
-            type="text"
-            placeholder="Увлечения (через запятую)"
-            value={hobbies}
-            onChange={(e) => setHobbies(e.target.value)}
-          />
-        </label>
-        {photo && <img src={photo} alt="Person" />}
-        <button onClick={capturePhoto}>Сделать фото</button>
-        <button onClick={exportToJson}>Экспорт в JSON</button>
-        <button onClick={exportToPdf}>Экспорт в PDF</button>
-        <button onClick={handleAddUser}>Добавить в список</button>
-        <button
-          onClick={(e) => {
-            e.preventDefault();
-            setIsFormVisible(false);
-          }}
-        >
-          Отмена
-        </button>
-      </form>
-    </div>
-  );
-};
+	const handleAddUser = (e) => {
+		e.preventDefault()
+		const personData = {
+			id: currentId,
+			fullName,
+			birthdate,
+			gender,
+			hobbies: hobbies.split(',').map((hobby) => hobby.trim()),
+			image,
+		}
+		addUser(personData)
+		setCurrentId(currentId + 1)
+		setCurrentUser({
+			id: '',
+			fullName: '',
+			birthdate: '',
+			hobbies: '',
+			gender: '',
+			image: null,
+		})
+		setMediaStream(null)
+
+		setIsFormVisible(false)
+	}
+
+	const handleEditUser = (e) => {
+		e.preventDefault()
+		const personData = {
+			id,
+			fullName,
+			birthdate,
+			gender,
+			hobbies: hobbies.split(',').map((hobby) => hobby.trim()),
+			image,
+		}
+		editUser(personData)
+		setCurrentUser({
+			id: '',
+			fullName: '',
+			birthdate: '',
+			hobbies: '',
+			gender: '',
+			image: null,
+		})
+
+		setIsFormVisible(false)
+	}
+
+	if (!isFormVisible) {
+		return
+	}
+
+	return (
+		<div className='personFormWrapper'>
+			<form className='personForm' id='personForm'>
+				<label>
+					ФИО
+					<input
+						type='text'
+						placeholder='ФИО'
+						value={fullName}
+						onChange={(e) =>
+							setCurrentUser({
+								...currentUser,
+								fullName: e.target.value,
+							})
+						}
+					/>
+				</label>
+
+				<label>
+					Дата рождения
+					<input
+						type='date'
+						placeholder='Дата рождения'
+						value={birthdate}
+						onChange={(e) =>
+							setCurrentUser({
+								...currentUser,
+								birthdate: e.target.value,
+							})
+						}
+					/>
+				</label>
+
+				<fieldset>
+					<legend>Выберите пол</legend>
+					<div>
+						<label>
+							<input
+								checked={gender === 'male'}
+								type='radio'
+								value='male'
+								name='gender'
+								onChange={(e) =>
+									setCurrentUser({
+										...currentUser,
+										gender: e.target.value,
+									})
+								}
+							/>
+							Мужской
+						</label>
+						<label>
+							<input
+								checked={gender === 'female'}
+								type='radio'
+								value='female'
+								name='gender'
+								onChange={(e) =>
+									setCurrentUser({
+										...currentUser,
+										gender: e.target.value,
+									})
+								}
+							/>
+							Женский
+						</label>
+					</div>
+				</fieldset>
+
+				<label>
+					Увлечения
+					<input
+						type='text'
+						placeholder='Увлечения (через запятую)'
+						value={hobbies}
+						onChange={(e) =>
+							setCurrentUser({
+								...currentUser,
+								hobbies: e.target.value,
+							})
+						}
+					/>
+				</label>
+				<div>
+					{image ? (
+						<div>
+							<img
+								style={{ width: '90%', marginBottom: 15 }}
+								src={image}
+								alt='Captured'
+							/>
+							<button
+								onClick={(e) => {
+									e.preventDefault()
+									retakeImage()
+								}}
+							>
+								Переснять
+							</button>
+						</div>
+					) : (
+						<video
+							style={{ width: '90%', marginBottom: 15 }}
+							ref={videoRef}
+							autoPlay
+						></video>
+					)}
+					{!image && (
+						<button
+							style={{ marginRight: 15 }}
+							onClick={(e) => {
+								e.preventDefault()
+								startVideo()
+							}}
+						>
+							Включить камеру
+						</button>
+					)}
+					{mediaStream && !image && (
+						<button
+							onClick={(e) => {
+								e.preventDefault()
+								captureImage()
+							}}
+						>
+							Сделать фото
+						</button>
+					)}
+				</div>
+				<div
+					style={{
+						display: 'flex',
+						justifyContent: 'center',
+						gap: 15,
+						flexWrap: 'wrap',
+					}}
+				>
+					<button style={{ width: '50%' }} onClick={exportToJson}>
+						Экспорт в JSON
+					</button>
+					<button style={{ width: '45%' }} onClick={exportToPdf}>
+						Экспорт в PDF
+					</button>
+					{!currentUser.id ? (
+						<button
+							style={{ width: '50%' }}
+							onClick={handleAddUser}
+						>
+							Добавить в список
+						</button>
+					) : (
+						<button
+							style={{ width: '50%' }}
+							onClick={handleEditUser}
+						>
+							Сохранить
+						</button>
+					)}
+
+					<button
+						style={{ width: '45%' }}
+						onClick={(e) => {
+							e.preventDefault()
+							setIsFormVisible(false)
+							setCurrentUser({
+								id: '',
+								fullName: '',
+								birthdate: '',
+								hobbies: '',
+								gender: '',
+								image: null,
+							})
+							setMediaStream(null)
+						}}
+					>
+						Отмена
+					</button>
+				</div>
+			</form>
+		</div>
+	)
+}
